3c8fa7718de3256e25a0ddedc8779bb9f45e6a93
John Parker <jparker@urgetopunt.com>
2017-07-13 00:35:17 -0700

    handle low-level request exceptions such as timeouts

--- therminator/api.py
+++ therminator/api.py
@@ -5,7 +5,7 @@ import json
 import logging
 import requests
 
-def write(data, endpoint, api_key):
+def write(data, endpoint, api_key, timeout=30):
     """Post data to API server.
 
     Keyword arguments:
@@ -15,20 +15,24 @@ def write(data, endpoint, api_key):
     """
     logger = logging.getLogger(__name__)
     logger.debug('Started posting data')
-    response = requests.post(
-        endpoint,
-        headers={
-            'Authorization': api_key,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        data=json.dumps(data),
-    )
-    if response.ok:
-        time = response.elapsed.total_seconds()
-        logger.info('Data posted to API: {}'.format(response.reason))
-        logger.debug('Finished posting data ({:.1f}s)'.format(time))
-    else:
-        reason = response.reason
-        message = response.json().get('error')
-        logger.warning('Failed to post data: {}: {}'.format(reason, message))
+    try:
+        response = requests.post(
+            endpoint,
+            headers={
+                'Authorization': api_key,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            data=json.dumps(data),
+            timeout=timeout,
+        )
+        if response.ok:
+            time = response.elapsed.total_seconds()
+            logger.info('Data posted to API: {}'.format(response.reason))
+            logger.debug('Finished posting data ({:.1f}s)'.format(time))
+        else:
+            reason = response.reason
+            message = response.json().get('error')
+            logger.warning('Failed to post data: {}: {}'.format(reason, message))
+    except requests.exceptions.RequestException as e:
+        logger.warning('Failed to post data: {!r}'.format(e))
