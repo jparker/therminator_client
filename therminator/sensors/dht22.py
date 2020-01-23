3a4ce3024147a4a706f1a5e8fa42d5428751e36b
John Parker <jparker@urgetopunt.com>
2020-01-23 07:52:11 +0000

    add tolerance to dht22

--- therminator/sensors/dht22.py
+++ therminator/sensors/dht22.py
@@ -5,16 +5,24 @@ import logging
 import time
 import Adafruit_DHT as DHT
 
-def read(pin, threshold=32):
+def read(pin, threshold=32, cache='/dev/shm/dht22', tolerance=5):
     """Return the external temperature and humidity.
     
     Keyword arguments:
     pin -- the GPIO pin connected to the DHT22's data pin
     """
     logger = logging.getLogger(__name__)
+
+    ref = read_reference(cache, logger)
     logger.debug('Started reading sensor')
     t1 = time.time()
     humidity, temp = DHT.read_retry(DHT.DHT22, pin)
+    write_reference(cache, temp, logger)
+    if ref is not None and abs(temp - ref) > tolerance:
+        logger.warning('reading offset exceeds {:.0f}C: retrying'.format(tolerance))
+        logger.debug('last reading was {:.1f}C, new reading is {:.1f}C'.format(ref, temp))
+        humidity, temp = DHT.read_retry(DHT.DHT22, pin)
+        write_reference(cache, temp, logger)
     t2 = time.time()
     if temp is None or humidity is None:
         raise RuntimeError('DHT22 sensor returned incomplete data')
@@ -26,6 +34,24 @@ def read(pin, threshold=32):
     logger.info('temp={:.1f}C humidity={:.1f}%'.format(temp, humidity))
     logger.debug('Finished reading sensor ({:.1f}s)'.format(t2-t1))
     return temp, humidity
+
+def read_reference(cache, logger):
+    if cache is not None:
+        logger.debug('Read reference temperature from cache')
+        try:
+            with open(cache, 'r') as f:
+                data = f.readline()
+                return float(data)
+        except FileNotFoundError:
+            logger.warning('Reference cache file does not exist')
+        except ValueError:
+            logger.warning('Could not parse reference temeprature: {!r}'.format(data))
+
+def write_reference(cache, ref, logger):
+    if cache is not None:
+        logger.debug('Write new reference temperature to cache')
+        with open(cache, 'w+') as f:
+            f.write('{:f}\n'.format(ref))
 
 
 if __name__ == '__main__':
